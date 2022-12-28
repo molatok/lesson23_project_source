a5c3c82c0d9c70266b5bc6ac8bc2598ffdc85abc
molatok <molatok@ya.ru>
2022-12-28 22:30:51 +0300

    mypy

--- func.py
+++ func.py
@@ -1,16 +1,16 @@
 import re
 
 
-def filter(data, value: str):
+def filter(data, value: str) -> list:
     return list(_ for _ in data if value in _)
 
 
-def unique(data):
+def unique(data) -> list:
     unique_data = set(data)
     return list(unique_data)
 
 
-def sort(data, asc):
+def sort(data, asc) -> list:
     if asc == 'desc':
         return list(data.sort())
     else:
@@ -21,11 +21,10 @@ def limit(data, value: int):
     return data[0:value]
 
 
-def map(data, value: int):
+def map(data, value: int) -> list:
     return list(_.split(" ")[int(value)] for _ in data)
 
 
-def regex(data, value):
-    return list(re.findall(f'{data}', value))
-
+def regex(data, value: str) -> list:
+    return re.findall(data, value)
 
